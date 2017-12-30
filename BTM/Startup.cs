912a213d2257dd85cc1f496dcaf78b70974e5f2a
Mehedi Hasan <hmehedi995@gmail.com>
2017-12-30 21:54:26 +0600

    Initial commit

---
+++ BTM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BTM.Startup))]
+namespace BTM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
