8517e6c886ff95a2d9dc7932b0bac59d640fcc86
Mehedi Hasan <hmehedi995@gmail.com>
2018-01-01 20:29:08 +0600

    -db

--- BTMRepository/ISpecialUserRepository.cs
+++ BTMRepository/ISpecialUserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BTMEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace BTMRepository
 {
-    interface ISpecialUserRepository
+    public interface ISpecialUserRepository:IRepository<SpecialUser>
     {
+
     }
 }
